171c21eeacbba11392695e32f5deeba90f791580
IBM <IBM@DEVOPS_01>
2018-07-24 14:48:27 +0530

    uploading project

---
+++ Jenkinsbdddemo/features/mytest.feature
@@ -0,0 +1,6 @@
+Feature: To test the shopping service
+@login
+Scenario: To validate login application
+Given Login application should be available
+When validate login application
+Then login should be successfull
---
+++ Jenkinsbdddemo/src/test/java/Stepdefenition/Stepdefenition.java
@@ -0,0 +1,37 @@
+package Stepdefenition;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class Stepdefenition {
+	WebDriver driver;
+	String title;
+	
+	@Given("^Login application should be available$")
+	public void login_application_should_be_available() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver=new FirefoxDriver();
+		   driver.get("https://opensource-demo.orangehrmlive.com");
+	}
+
+	@When("^validate login application$")
+	public void validate_login_application() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		title=driver.getTitle();
+		System.out.println(title);
+	}
+
+	@Then("^login should be successfull$")
+	public void login_should_be_successfull() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		Assert.assertEquals("OrangeHRM",title);
+	}
+
+}
